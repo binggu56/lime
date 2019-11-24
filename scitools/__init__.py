37dbdc62cfb92b7f18de9fc44999ab7ab5d9573f
Bing Gu <binggu56@gmail.com>
2019-11-23 18:34:30 -0800

    delete old files

--- scitools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .style import set_style
-from .noise import cnoise
-from .spin_boson import Spin_boson
-from .Floquet import quasiE
-from .tight_binding import TightBinding
-from .common import delta, dagger
-from .phys import *
-from .units import au2ev
-from .oqs import *
--- scitools/units.py
+++
@@ -1,6 +0,0 @@
-
-au2fs = 2.41888432651e-2 # femtoseconds
-au2k = 315775.13 #K
-au2ev = 27.2116
-
-
